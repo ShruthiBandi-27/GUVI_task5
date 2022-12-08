214ecb57d6b47e645334d41e0b20219b4bfd04db
ShruthiBandi-27 <80580324+ShruthiBandi-27@users.noreply.github.com>
2022-12-08 23:47:14 +0530

    Create odd_arrow.js

---
+++ odd_arrow.js
@@ -0,0 +1,11 @@
+var result = (a) => {
+    let b = [];
+    for(let i =0 ;i<a.length;i++){
+        if(a[i] % 2 !==0)
+            //console.log(a[i]);
+            b.push(a[i]);
+    }
+    return b;
+}
+
+console.log(result([1,2,3,4,5]))
